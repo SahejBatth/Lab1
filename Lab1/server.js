8e11714ab4dd4ac0b78cf55bdf1f084adba7a9dc
SahejBatth <50597515+SahejBatth@users.noreply.github.com>
2019-09-13 19:01:59 -0400

    comit 2

--- Lab1/server.js
+++ Lab1/server.js
@@ -5,5 +5,7 @@ var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello world');
+    res.end('hii there');
+    res.end('sahej preet');
     
 }).listen(port);
